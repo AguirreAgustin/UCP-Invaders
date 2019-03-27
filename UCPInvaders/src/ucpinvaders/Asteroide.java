6d9fdd03a1a0f81b7387c6d4636582e119423363
Agustin Aguirre <ardagustin14@gmail.com>
2019-03-27 13:25:27 -0300

    Con clase Asteriod

---
+++ UCPInvaders/src/ucpinvaders/Asteroide.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ucpinvaders;
+
+/**
+ *
+ * @author Agustin
+ */
+public class Asteroide {
+    private int velocidad;
+    
+    public Asteroide(int pVelocidad){
+        
+        this.setVelocidad(pVelocidad);
+        
+    }
+
+    /**
+     * @return the velocidad
+     */
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    /**
+     * @param velocidad the velocidad to set
+     */
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+    
+}
